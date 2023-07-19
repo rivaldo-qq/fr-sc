31b09ed9223173d13ce4dde094dbd1cb62c18321
Rivaldo Ramadhan <rivaldorizqiramadhan@gmail.com>
2023-07-19 19:37:50 +0700

    new up

---
+++ build/precache-manifest.0f2caca68b31b79a9ccc5334a69f3e35.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbd00d207de5cd9ff39e2a3c43d9d48f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81bcb959c47e4469a373",
+    "url": "/static/css/2.ae4320a7.chunk.css"
+  },
+  {
+    "revision": "ca8a68ef43d64e2faaf6",
+    "url": "/static/css/main.f8a44285.chunk.css"
+  },
+  {
+    "revision": "81bcb959c47e4469a373",
+    "url": "/static/js/2.09103481.chunk.js"
+  },
+  {
+    "revision": "e99b72212563bbc6a0b87716f1a0a8e2",
+    "url": "/static/js/2.09103481.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca8a68ef43d64e2faaf6",
+    "url": "/static/js/main.194a734c.chunk.js"
+  },
+  {
+    "revision": "8a70aafdb341e4e96a4f",
+    "url": "/static/js/runtime-main.1cdbaa73.js"
+  },
+  {
+    "revision": "3366f1365b8130f7b2eff3d2b3bba490",
+    "url": "/static/media/completed.3366f136.jpg"
+  },
+  {
+    "revision": "67e25a39beb992b5476840cc2ce5aa21",
+    "url": "/static/media/icon-calendar.67e25a39.svg"
+  },
+  {
+    "revision": "eacd8f56422fa4e01a3a7b6005955918",
+    "url": "/static/media/img-hero.eacd8f56.jpg"
+  },
+  {
+    "revision": "11a62c712d17eaee33a02927afd01988",
+    "url": "/static/media/star.11a62c71.svg"
+  }
+]);
